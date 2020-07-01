9bbed9573212b262f151d7738421b100b121e240
cs91chris <cs91chris@voidbrain.me>
2020-07-01 23:19:58 +0200

    add Increment and Decrement

---
+++ src/main/java/it/greenvulcano/gvesb/adapter/redis/operations/Decrement.java
@@ -0,0 +1,29 @@
+
+package it.greenvulcano.gvesb.adapter.redis.operations;
+
+import org.w3c.dom.Node;
+
+import it.greenvulcano.configuration.XMLConfig;
+import it.greenvulcano.configuration.XMLConfigException;
+import it.greenvulcano.gvesb.buffer.GVBuffer;
+import it.greenvulcano.util.metadata.PropertiesHandler;
+import it.greenvulcano.util.metadata.PropertiesHandlerException;
+
+public class Decrement extends BaseOperation {
+	protected String key = null;
+	
+	@Override
+	public void init(Node node) throws XMLConfigException {
+		super.init(node);
+		
+		key = XMLConfig.get(node, "@key");
+	}
+	
+	@Override
+	public String perform(GVBuffer gvBuffer) throws PropertiesHandlerException {
+		super.perform(gvBuffer);
+		
+		key = PropertiesHandler.expand(key, gvBuffer);
+		return Long.toString(client.decr(key));
+	}
+}
